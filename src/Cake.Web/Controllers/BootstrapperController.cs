5a4290a80081f0a42eaaddce1b2f5e2f31e4477d
Patrik Svensson <patrik@patriksvensson.se>
2015-09-06 13:48:31 +0200

    Added bootstrapper redirects.

---
+++ src/Cake.Web/Controllers/BootstrapperController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Cake.Web.Controllers
+{
+    public class BootstrapperController : Controller
+    {
+        public ActionResult Windows()
+        {
+            return Redirect("https://raw.githubusercontent.com/cake-build/bootstrapper/master/res/scripts/build.ps1");
+        }
+
+        public ActionResult Linux()
+        {
+            return Redirect("https://raw.githubusercontent.com/cake-build/bootstrapper/master/res/scripts/build.sh");
+        }
+
+        // ReSharper disable once InconsistentNaming
+        public ActionResult OSX()
+        {
+            return Redirect("https://raw.githubusercontent.com/cake-build/bootstrapper/master/res/scripts/build.sh");
+        }
+    }
+}
