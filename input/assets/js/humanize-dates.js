6c36b8ca069af9ce6ce38179c38624ad0d2c76d4
Mattias Karlsson <matkar@wcom.se>
2026-06-20 22:18:59 +0200

    Polish client-side date humanization for extension pages
    Remove the unused Humanizer.Core Wyam package now that relative dates
    are computed in the browser. Add week-based intervals for closer parity
    with the old output, datetime attributes on <time> elements for
    accessibility, and minor template/JS formatting cleanup.

--- input/assets/js/humanize-dates.js
+++ input/assets/js/humanize-dates.js
@@ -4,6 +4,7 @@
         var intervals = [
             [31536000, 'year'],
             [2592000,  'month'],
+            [604800,   'week'],
             [86400,    'day'],
             [3600,     'hour'],
             [60,       'minute']
